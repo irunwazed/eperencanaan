7fed2a3e5adc708048fba016b4892369851166f6
irunwazed <irunwazed@gmail.com>
2019-05-19 20:48:37 +0700

    v.13

---
+++ frondend/rkpd/config.js
@@ -0,0 +1,7 @@
+var config ={
+    "baseUrl": "http://localhost/ci/frondend/rkpd/", 
+    "apiRoot": "http://localhost/ci/eperencanaan/", 
+    "apiMusrenbang": "http://localhost/ci/perencanaan/",
+    "apiSatuanHarga": "http://localhost/ci/satuanharga/",
+    "index": 1
+};
